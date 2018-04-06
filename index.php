3f370f273c9037c20f60c9207aabe2f05a874a84
Stella97 <31676244+Stella97@users.noreply.github.com>
2018-04-06 08:34:45 +0800

    Footer Link Updated
    Footer Element About Us and Contact Us link updated

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
 					<a href="./index.php" class="brand-logo">Pinocone</a>
 					<ul id="nav" class="right">
 						<li><a href="food_menu.php">Food Menu</a></li>
-						<li><a href="#">About Us</a></li>
+						<li><a href="about_us.php">About Us</a></li>
 						<li><a href="<?php include 'includes/index_user_nav_tab.php'?>
 					</ul>
 				</div>
@@ -130,8 +130,8 @@
 					<div class="col l4 offset-l2 s12">
 						<h5 class="white-text">Links</h5>
 						<ul>
-							<li><a class="grey-text text-lighten-3" href="#!">About Us</a></li>
-							<li><a class="grey-text text-lighten-3" href="#!">Contact</a></li>
+							<li><a class="grey-text text-lighten-3" href="about_us.php">About Us</a></li>
+							<li><a class="grey-text text-lighten-3" href="contact_us.php">Contact Us</a></li>
 						</ul>
 					</div>
 				</div>
