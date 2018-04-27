d36771f3c0b455205d19fd939ddffccd8dbae6db
Kennethyi <kennethyii@hotmail.com>
2018-04-27 10:11:54 +0800

    update track and update prt2

---
+++ includes/warm_food_menu_data.php
@@ -0,0 +1,19 @@
+<?php
+	header("Access-Control-Allow-Origin: *");
+	$conn = new mysqli("localhost", "root", "", "pinocone");
+	$result = $conn->query("SELECT * FROM warm_food_menu");
+	
+	$outp = "";
+	while ($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+		if ($outp != "") {$outp .= ",";}
+		$outp .= '{"food_package":"'	. $rs["food_package"]		. '",';
+		$outp .= '"desc":"'				. $rs["description"]		. '",';
+		$outp .= '"info":"'				. $rs["info"]				. '",';
+		$outp .= '"price":"'			. $rs["price"]				. '",';
+		$outp .= '"imgindex":'			. $rs["imgindex"]			. '}';
+	}
+	$outp = '{"food_package":['.$outp.']}';
+	$conn->close();
+	
+	echo($outp);
+?>
---
+++ js/warm_food_menu.js
@@ -0,0 +1,12 @@
+/*jslint white:true */
+/*global angular */
+var app = angular.module("food_menu", []);
+app.controller("food_ctrl", ["$scope", "$http",
+    function($scope, $http) {
+		$http({
+			method: 'get',
+			url: './includes/warm_food_menu_data.php'
+		}).then(function successCallback(response) {
+			$scope.food_package = response.data.food_package;
+		});
+    }]);
