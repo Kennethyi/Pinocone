ef9fae326074e81e25797be46ac6f94f40fd6edb
Stella97 <31676244+Stella97@users.noreply.github.com>
2018-05-01 03:31:49 +0800

    The buy info for cold food menu
    The buy info for cold food menu is updated

---
+++ cold_food_buy_info.php
@@ -0,0 +1,259 @@
+<?php
+	session_start();
+	if (!isset($_SESSION['username'])) {
+		$_SESSION['msg'] = "You must log in first";
+		header('location: login.php');
+	}
+	if (isset($_GET['logout'])) {
+		session_destroy();
+		unset($_SESSION['username']);
+		header("location: login.php");
+	}
+	if (($food_package = $_GET['food-package']))
+$_SESSION['food-package'] = $food_package;
+        
+       // if(!($food_package =$_GET['food-package']))
+        //{header('location:warm_food_menu.php')}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Food Menu</title>
+		<!-- Materialize CSS -->
+		<link rel="stylesheet" href="css/materialize.css">
+
+		<!-- Materialize JavaScript -->
+		<script src="js/materialize.js"></script>
+		
+		<!-- AngularJS -->
+		<script src="js/angular.js"></script>
+		
+		<!-- JQuery -->
+		<script src="js/jquery.js"></script>
+		
+		<!-- Material icons -->
+		<link href="css/material-icon.css" rel="stylesheet">
+		
+		<!-- Food Buy CSS -->
+		<link rel="stylesheet" href="css/food_buy.css">
+		
+		<!-- Food Package Data -->
+		<script src="js/cold_food_menu.js"></script>
+		
+	</head>
+	
+	<body class="grey lighten-5" data-ng-app="food_menu">
+		<header>
+			<nav>
+				<div class="nav-wrapper z-depth-3">
+					<a href="./index.php" class="brand-logo">Pinocone</a>
+					<ul id="nav" class="right">
+						<li><a href="./food_menu.php">Food Menu</a></li>
+						<li><a href="about_us.php">About Us</a></li>
+                        <li><a href="<?php include 'includes/user_nav_tab.php'?>"></a></li>
+					</ul>
+				</div>
+				<div class="nav-wrapper z-depth-0 grey lighten-4">
+					<div class="row">
+						<a href="./index.php" id="first_breadcrumb" class="breadcrumb red-text text-darken-1">Home</a>
+						<a href="./food_menu.php" class="breadcrumb red-text text-darken-1 red-darken-1">Food Menu</a>
+                        <a href="./cold_food_menu.php" class="breadcrumb red-text text-darken-1 red-darken-1">Cold Food Menu</a>
+						<a href="#" class="breadcrumb red-text text-darken-1 red-darken-1">Food Package <?php echo str_replace("'", "", $_SESSION['food-package']);?></a>
+					</div>
+				</div>
+			</nav>
+		</header>
+		
+			<div data-ng-controller="food_ctrl" id="content" class="container" style="margin-top: 82px;">
+			
+				<div id="parentcard" class="card-panel z-depth-3">
+					<p id="cardtitle" class="row red lighten-1 center white-text">Food Package <?php echo str_replace("'", "", $_SESSION['food-package']);?></p>
+					
+					<h5 class="outerh5">Your selection information</h5>
+					
+					<div id="preview" data-ng-init="counts=[0,1,2,3]" class="row">
+						<div data-ng-repeat="loop in counts" class="col s3">
+							<img src="img/food/cool{{food_package[<?php echo $_SESSION['food-package'];?> - 1].imgindex + $index}}.jpg"/>
+						</div>
+					</div>
+					
+					<blockquote>
+					{{food_package[<?php echo $_SESSION['food-package'];?> - 1].info}}
+					</blockquote>
+					
+					<div id="pricecard" class="card-panel red lighten-1">
+						<p class="white-text">{{food_package[<?php echo $_SESSION['food-package'];?> - 1].price}}</p>
+					</div>
+					
+					<div class="divider"></div>
+					
+					<h5 class="outerh5" >Your basic information</h5>
+					
+					<div id="parentpaymentform" class="row">
+						<form class="col s12">
+							<div class="row">
+								<div class="input-field col s5">
+									<input id="first_name" type="text" class="validate" name="first_name" required="">
+									<label for="first_name">First Name</label>
+								</div>
+								
+								<div class="input-field col s5 offset-s1">
+									<input id="last_name" type="text" class="validate" name="last_name" required="">
+									<label for="last_name">Last Name</label>
+								</div>
+							</div>
+							
+							<h5 id="innerh5">Your address details</h5>
+							
+							<div class="row">
+								<div id="paymentform" class="input-field col s11">
+									<input id="address1" type="text" class="validate" name="address1" required="">
+									<label for="address1">Address Line 1</label>
+								</div>
+							</div>
+							
+							<div class="row">
+								<div id="paymentform" class="input-field col s11">
+									<input id="address2" type="text" class="validate" name="address2" required="">
+									<label for="address2">Address Line 2</label>
+								</div>
+							</div>
+							
+							<div class="row">
+								<div id="paymentform" class="input-field col s11">
+									<input id="city" type="text" class="validate" name="city" required="">
+									<label for="city">City</label>
+								</div>
+							</div>
+							
+							<div class="row">
+								<div class="input-field col s5">
+									<input id="statee" type="text" class="validate" name="statee" required="">
+									<label for="statee">State</label>
+								</div>
+								
+								<div class="input-field col s5 offset-s1">
+									<input id="postcode" type="number" class="validate" name="postcode" required="">
+									<label for="postcode">Postcode</label>
+								</div>
+							</div>
+							
+							<button id="save_details" class="btn-large waves-effect waves-light red lighten-1 white-text right" type="submit">Save
+								<i class="material-icons right">save</i>
+							</button>
+							
+							<button id="rstbtn" class="btn-large waves-effect waves-light red lighten-1 white-text right" type="reset">reset
+								<i class="material-icons right">clear</i>
+							</button>
+							
+							<button id="bckbtn" class="btn-large waves-effect waves-light red lighten-1 white-text left" onclick="window.history.go(-1); return false;" type="button">go back
+								<i class="material-icons left">arrow_back</i>
+							</button>
+							
+						</form>
+					</div>
+					
+				</div>
+				
+			</div>
+			<!-- Credit Card Form Structure -->
+			<div style="overflow:visible;" id="modal1" class="modal">
+				<div id="modalbox" class="modal-content">
+					<h4 class="center">Confirm Purchase</h4>
+					<form id="creditcard" class="col s12">
+						<div style="margin-left:0px; margin-right:0px;" class="row">
+							<div class="input-field col s8">
+								<label style="margin-left:-9px;" for="Owner">Owner</label>
+								<input id="Owner" type="text" class="validate" name="Owner">
+							</div>
+
+							<div class="input-field col s1 offset-s1">
+								<label style="margin-left:-9px;" for="CVV">CVV</label>
+								<input id="CVV" type="text" class="validate" name="CVV">
+							</div>
+						</div>
+						
+						<div style="margin-left:0px; margin-right:0px;" class="row">
+							<div class="input-field col s10">
+								<label style="margin-left:-9px;" for="cardnum">Card Number</label>
+								<input id="cardnum" type="text" class="validate" name="cardnum">
+							</div>
+						</div>
+						
+						<div style="margin-left:0px; margin-right:0px;" class="row">
+							<div class="input-field col s1">
+								<select id="month">
+									<option value="01" selected>01</option>
+									<option value="02">02</option>
+									<option value="03">03</option>
+									<option value="04">04</option>
+									<option value="05">05</option>
+									<option value="06">06</option>
+									<option value="07">07</option>
+									<option value="08">08</option>
+									<option value="09">09</option>
+									<option value="10">10</option>
+									<option value="11">11</option>
+									<option value="12">12</option>
+								</select>
+								<label style="margin-left:-10px;" for="month">Month</label>
+							</div>
+							
+							<div class="input-field col s1 offset-s1">
+								<select id="year">
+									<option value="2018" selected>2018</option>
+									<option value="2019">2019</option>
+									<option value="2020">2020</option>
+									<option value="2021">2021</option>
+									<option value="2022">2022</option>
+									<option value="2023">2023</option>
+									<option value="2024">2024</option>
+								</select>
+								<label style="margin-left:-10px;" for="year">Year</label>
+							</div>
+							
+							<div style="width:auto; margin-top:5px;" class="col s3 offset-s3">
+								<img style="width:auto;" src="img/visa.jpg"/>
+								<img style="width:auto;" src="img/mastercard.jpg"/>
+								<img style="width:auto;" src="img/amex.jpg"/>
+							</div>
+						</div>
+					</form>
+				</div>
+				<div class="modal-footer center-align">
+					<div class="center-align">
+						<button id="save_order" style="width:95%;" class="modal-action modal-close btn waves-effect waves-light red lighten-1 white-text">Confirm
+						</button>
+					</div>
+				</div>
+			</div>
+
+		<footer class="page-footer z-depth-3">
+			<div class="container">
+				<div class="row">
+					<div class="col l6 s12 footer-h5">
+						<h5 class="white-text">Pinocone</h5>
+						<p class="grey-text text-lighten-4">Online Food Catering.</p>
+					</div>
+					<div class="col l4 offset-l2 s12 footer-h5">
+						<h5 class="white-text">Links</h5>
+						<ul>
+							<li><a class="grey-text text-lighten-3" href="about_us.php">About Us</a></li>
+							<li><a class="grey-text text-lighten-3" href="contact_us.php">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="footer-copyright">
+				<div class="container">
+				&copy; 2018 Pinocone
+				<a class="grey-text text-lighten-4 right" href="#!">Pinocone</a>
+				</div>
+			</div>
+		</footer>
+		<!-- Food Buy Info JS -->
+		<script src="js/food_buy_info.js"></script>
+
+	</body>
+</html>
